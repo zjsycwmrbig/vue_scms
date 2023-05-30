9fdced8bf41bd3bbd9b76577e5a82ad8fca4777f
zjsycwmrbig <2057544748@qq.com>
2023-05-30 11:03:29 +0800

    5/30

--- src/components/AsidePart/ClashPart.vue
+++ src/components/AsidePart/ClashPart.vue
@@ -1,23 +1,68 @@
 <template>
     <!-- 加个动画，绑定一下显示的内容，通过调用一个函数，给store中数据赋值，渲染出来 -->
+    <transition
 
-    <div v-if="clashStore.clashFlag" class="clashbox">
-      <el-icon class="close" @click="clashStore.clashFlag = false"><Close /></el-icon>
-      <el-text>出现冲突!</el-text>
-      <el-tabs>
-        <el-tab-pane v-for="(item,index) in clashStore.clashList" :label="item.netName == null ? '用户' + index : item.netName" :key="index">
-          <ClashItem :index="index"/>
-        </el-tab-pane>
-      </el-tabs>
-  </div>
+      enter-active-class="animated animate__slideInRight"
+      leave-active-class="animated animate__slideOutRight"
+    >
+      <div v-if="clashStore.clashFlag" class="clashbox">
+        <el-icon class="close" @click="clashStore.clashFlag = false"><Close /></el-icon>
+        
+        <div class="title">
+          出现冲突
+        </div>
+        
+        <div class="tip">
+            以下用户和您发生了冲突(点击查看用户空闲时间)
+        </div>
+
+        <div class="clashuserbox" style="display:flex; flex-direction:column; justify-content: space-around; padding: 5px; width: 90%; margin: 5px auto;">
+          <div class="tag">
+            <el-tag
+                :type="item.isOwner == 1 ? 'primary' : 'info'"
+                effect="dark"
+                round
+                v-for="(item,index) in clashStore.clashList"
+                :key="index"
+                @click="null"
+            >
+              {{ item.netName == null ? item.username : item.netName  }}
+            </el-tag>
+          </div>
+
+        </div>
+
+        <div class="tip">
+          和本身发生冲突的事件
+        </div>
+        <div class="clasheventbox">
+          <!-- 冲突盒子 -->
+          <div class="clashorg" v-for="(item,index) in clashStore.clashList" :key="index">
+            <div class="clashnum">
+              和 {{ item.netName == null ? item.username : item.netName  }} 至少有{{item.clashNum}}个事件发生冲突
+            </div>
+
+            <!-- 本身就渲染,不是就不渲染 -->
+            <div class="clashitem" v-if="item.isOwner == 1">
+              <div class="clashlist" v-for="(clashItem,clashIndex) in item.list" :key="clashIndex">
+                <ClashData :data="clashItem"/>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </transition>
+    
 </template>
 
 <script>
 import { useClashStore } from '@/store/pinia'
-import ClashItem from './ClashItem.vue'
+// import ClashItem from './ClashItem.vue'
+import ClashData from './ClashData.vue'
     export default {
       components: {
-        ClashItem
+        ClashData
       },
       setup(){
         let clashStore = useClashStore()
@@ -38,8 +83,10 @@ import ClashItem from './ClashItem.vue'
     bottom: 5vh;
     height: 50vh;
     width: 30vw;
-    border-radius: 3px;
+    border-radius: 5px;
     background-color: #fef1f1;
+    padding: 5px;
+    border:2px solid #f56c6c;
   }
   .close{
     position: absolute;
@@ -48,4 +95,43 @@ import ClashItem from './ClashItem.vue'
     font-size: 2rem;
     color: #f56c6c;
   }
+  .clashuserbox{
+    margin: 0.5vh 0;
+    width: 100%;
+    background-color: #CC9999;
+    border-radius: 4px;
+    padding: 0.5px 0;
+  }
+  .space{
+    margin: 2px 0;
+
+  }
+  .tip{
+    padding-left:4px ;
+    font-size: 1.5rem;
+    font-weight: bold;
+    border-left:4px solid #20a0ff;
+    color: #f56c6c;
+  }
+
+  .title{
+    text-align: center;
+    font-size: 2rem;
+    font-weight: bold;
+    color: #f56c6c;  
+    border-bottom: 2px solid #f56c6c;
+  }
+  
+  .clashnum {
+    font-size: 1.2rem;
+    font-weight: bold;
+    padding-left: 4px;
+    
+    margin: 3px 0;
+    
+    border-left:4px solid #20a0ff;
+    color: #444648;
+    margin: 2px 0;
+  }
+
 </style>
--- src/components/OperationPart/FindFree.vue
+++ src/components/OperationPart/FindFree.vue
@@ -5,53 +5,105 @@
         title="空闲时间"
         :show-close="true"
         class="form">
-        <el-text>有空吗,靓仔</el-text>
-        <el-row>
-        <el-input
-            v-model="form.key"
-            placeholder="请输入用户名或者组织名"
-        >
-        <template #prepend>
-        <el-select v-model="form.mode" placeholder="搜索模式">
-          <el-option label="用户空闲时间" value="0" />
-          <el-option label="组织空闲时间" value="1" />
-        </el-select>
-      </template>
-      
-      <template #append>
-        <el-button @click="freeTimeStore.FindFreeTime(form.key,form.mode)">
-            <el-icon><Search /></el-icon>
-        </el-button>
-      </template>
+        <div class="searchpart">
+            <div class="searchoption">
+                <div class="date">
+                <el-date-picker
+                  v-model="freeTimeStore.form.date"
+                  type="date"
+                  placeholder="选择日期"
+                  :default-value="new Date(time.GlobalTime)"
+                />
+            </div>
+            <div>
+                <div class="text">此后</div>
+            </div>
+            <div class="length">
+                <el-input-number v-model="freeTimeStore.form.length" :min="1" :max="7" />
+            </div>
+            <div>
+                <div class="text">
+                    天
+                </div>
+            </div>
+            
+            </div>
+            
+            <el-divider></el-divider>
+            
+            <div class="searchname">
+                <el-input
+                v-model="freeTimeStore.form.key"
+                placeholder="请输入用户名或者组织名"
+            >
 
-    </el-input>
-    </el-row>
+            <template #prepend>
+            
+            <el-select v-model="freeTimeStore.form.mode" placeholder="搜索模式">
+              <el-option label="用户空闲时间" value="0" />
+              <el-option label="组织空闲时间" value="1" />
+            </el-select>
 
+            </template>
+            
+            <template #append>
+              <el-button @click="freeTimeStore.FindFreeTime()">
+                  <el-icon><Search /></el-icon>
+              </el-button>
+            </template>
+            </el-input>
+            </div>
+            
+            
+            
+        </div>
+        
     </el-drawer>
 </template>
 
 <script>
-import { useOperationStore } from '@/store/pinia'
+import { useOperationStore, useTimeStore } from '@/store/pinia'
 import {useFindFreeTimeStore} from '@/store/pinia'
 import {reactive} from 'vue'
 export default {
     setup(){
         let option = useOperationStore()
         let freeTimeStore = useFindFreeTimeStore()
+        let time = useTimeStore()
+        // 查询空闲时间表单
         let form = reactive({
             key:"",
-            mode:""
+            mode:0,
+            date:"",
+            length:""
         })
-
+        
         return{
             option,
             freeTimeStore,
-            form
+            form,
+            time
         }
     }
 }
 </script>
 
-<style>
-
+<style scoped> 
+    .searchpart{
+        width: 100%;
+        border: 2px solid #272b32;
+        margin: 5px;
+        padding: 5px;
+        background-color: #fde3c7;
+    }
+    .searchoption{
+        display: flex;
+        justify-content: space-between;
+    }
+    .text{
+        font-size: 2rem;
+        color: #272b32;
+        line-height: 100%;
+        margin: auto 0;
+    }
 </style>
