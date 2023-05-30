492abace11dc6e23b5c518d6dfdee12de000ddb8
Daddy <byrzj@bupt.edu.cn>
2023-05-30 13:11:41 +0800

    修改导航展示

--- src/components/OperationPart/NavigationShow.vue
+++ src/components/OperationPart/NavigationShow.vue
@@ -7,7 +7,7 @@
         :size="'45%'"
     >
         <div class="map">
-            <div v-for="item in interpolatedPoints" :key="item.index">
+            <div v-for="item in store.interpolatedPoints" :key="item.index">
                 <template>
                     <div class="realpoint" :style="LocatePoint(item)"></div>
                 </template>
@@ -21,29 +21,12 @@
 <script>
 import { useMapStore } from '@/store/pinia';
 import { ref } from 'vue';
-import { onMounted } from 'vue';
 
 export default {
     setup() {
         const store = useMapStore()
-        const List =  ref(store.navigationList); // 假设导航路线是一个响应式引用
         const interpolatedPoints = ref([]); // 声明一个用于存储插值点的响应式引用
         // 当组件挂载完成后执行插值点生成逻辑
-        onMounted(() => {
-            const segments = List.value.length - 1;
-            for (let i = 0; i < segments; i++) {
-                const start = List.value[i];//start end 为pid
-                const end = List.value[i+1];
-                const numInterpolatedPoints = 10; // 插值点的数量
-                for (let j = 1; j <= numInterpolatedPoints; j++) {
-                    const x = store.points[start-1].x + (store.points[end-1].x-store.points[start-1].x)*(j / numInterpolatedPoints);
-                    // const x = start.x + (end.x - start.x) * (j / numInterpolatedPoints);
-                    const y = store.points[start-1].y + (store.points[end-1].y-store.points[start-1].y)*(j / numInterpolatedPoints);
-                    // const y = start.y + (end.y - start.y) * (j / numInterpolatedPoints);
-                    interpolatedPoints.value.push({ x, y });
-                }
-            }
-        });
 
         function LocatePoint(item){
                 let top = ((item.y * 47 / 100) + 0.25).toString()+'vw'
